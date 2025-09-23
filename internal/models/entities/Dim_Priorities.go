23e283845a5fc4dd83abb1508f161e5edbd20061
eduardofpaula <eduardo.paula01@fatec.sp.gov.br>
2025-09-22 21:32:23 -0300

    API-59-fix: remove Wheight Dim_Priorities struct

--- internal/models/entities/Dim_Priorities.go
+++ internal/models/entities/Dim_Priorities.go
@@ -3,6 +3,5 @@ package entities
 type Dim_Priorities struct {
 	PriorityKey   int64  `gorm:"column:PriorityKey;primaryKey;autoIncrement"`
 	Name          string `gorm:"column:Name;size:50"`
-	Wheight       int64  `gorm:"column:Wheight;size:4"`
 	PrioriryId_BK int64  `gorm:"column:PriorityId_BK;size:4"`
 }
